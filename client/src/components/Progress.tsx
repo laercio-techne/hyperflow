c133d065d5e2aac567b147d9df9e9afbc79be77b
Laercio Nogueira <laercio.nogueira@techne.com.br>
2024-05-08 13:58:25 -0300

    Reapply "remove useless"
    This reverts commit 4433b02327b609d16a79729d4b9d363f04249d4a.

--- client/src/components/Progress.tsx
+++ client/src/components/Progress.tsx
@@ -1,4 +1,4 @@
-import DotProgress from "components/DotProgress/DotProgress";
+import DotProgress from "components/DotProgress";
 import { TailSpin } from "react-loader-spinner";
 import React from "react";
 
