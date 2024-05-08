59d2f6a1009b9287518abd7836f7d9e0c8554ce4
Laercio Nogueira <laercio.nogueira@techne.com.br>
2024-05-08 13:25:00 -0300

    Revert "remove files useless"
    This reverts commit 9f509a911d70e80877a5f9344f74388787acfcad.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import RouteComponent from "./Routes";
+import App from "./App";
 import { store } from "store/store";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <RouteComponent />
+    <App>
+      <RouteComponent />
+    </App>
   </Provider>
 );
 
