4361e6a98d8a5fc947853a0bf9c0d5516c0def17
Laercio Nogueira <laercio.nogueira@techne.com.br>
2024-05-09 18:56:32 -0300

    config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import RouteComponent from "./Routes";
-import { store } from "store/store";
+import RouteComponent from "./routes";
+import { store } from "store";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <RouteComponent />
   </Provider>
 );
-
-
