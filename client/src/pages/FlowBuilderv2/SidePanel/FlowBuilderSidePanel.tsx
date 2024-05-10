8271a0e1576c5988a699c6f2a30609071a328be4
Laercio Nogueira <laercio.nogueira@techne.com.br>
2024-05-09 23:01:18 -0300

    fix modal

--- client/src/pages/FlowBuilderv2/SidePanel/FlowBuilderSidePanel.tsx
+++ client/src/pages/FlowBuilderv2/SidePanel/FlowBuilderSidePanel.tsx
@@ -11,10 +11,11 @@ import { useAppDispatch, useAppSelector } from "store/hooks";
 import deepCopy from "utils/deepCopy";
 import Button, {
   ButtonType,
-} from "../../../components/Buttonv2";
+} from "components/Buttonv2";
 import { NodeType } from "../FlowEditor";
 import FlowBuilderDeleteModal from "../Modals/FlowBuilderDeleteModal";
 import FlowBuilderSaveTrackerModal from "../Modals/FlowBuilderSaveTrackerModal";
+import { messageFixtures } from "../Nodes/MessageNode";
 import { MessageNodeData, NodeData } from "../Nodes/NodeData";
 import MessageSettings from "./settings/MessageSettings";
 import TimeDelaySettings from "./settings/TimeDelaySettings";
@@ -25,6 +26,7 @@ import WaitUntilSettings from "./settings/WaitUntilSettings";
 import JumpToSettings from "./settings/JumpToSettings";
 import { isEqual } from "lodash";
 import Modal from "components/Modal";
+import { FlowBuilderMessageRenameModal } from "../Modals/FlowBuilderMessageRenameModal";
 import { MessageType } from "types/Workflow";
 import { Node } from "reactflow";
 import ExperimentNodeSettings from "./settings/ExperimentNodeSettings";
@@ -36,12 +38,14 @@ export interface SidePanelComponentProps<T extends NodeData = NodeData> {
   showErrors: boolean;
 }
 
+interface FlowBuilderSidePanelProps {
+  className?: string;
+}
 
 const FlowBuilderSidePanel: any = ({ className }: any) => {
   const { nodes, sidePanelErrors } = useAppSelector(
     (state: any) => state.flowBuilder
   );
-
   const dispatch = useAppDispatch();
 
   const [selectedNode, setSelectedNode] = useState<
@@ -66,6 +70,23 @@ const FlowBuilderSidePanel: any = ({ className }: any) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [isError, setIsError] = useState(false);
   const [showErrors, setShowErrors] = useState(false);
+
+  const nodeTypeToNameMap: Record<NodeType, string> = {
+    [NodeType.START]: "Start",
+    [NodeType.EMPTY]: "Empty",
+    [NodeType.MESSAGE]: "Message",
+    [NodeType.JUMP_TO]: "Jump to",
+    [NodeType.EXIT]: "Exit",
+    [NodeType.TIME_DELAY]: "Time delay",
+    [NodeType.TIME_WINDOW]: "Time window",
+    [NodeType.WAIT_UNTIL]: "Wait until",
+    [NodeType.USER_ATTRIBUTE]: "User attribute",
+    [NodeType.INSERT_NODE]: "Insert",
+    [NodeType.TRACKER]: "Custom component",
+    [NodeType.MULTISPLIT]: "Multisplit",
+    [NodeType.EXPERIMENT]: "Experiment",
+    [NodeType.PUSH]: "Push",
+  };
 
   const nodeToSettingsComponentMap: Record<string, ReactNode> = {
     [NodeType.MESSAGE]: (
@@ -242,7 +263,7 @@ const FlowBuilderSidePanel: any = ({ className }: any) => {
         nodeData.tracker.trackerTemplate.id !==
           selectedNode.data.tracker.trackerTemplate.id &&
         nodes.some(
-          (node: any) =>
+          (node) =>
             node.data.type === NodeType.TRACKER &&
             node.id !== selectedNode.id &&
             node.data.tracker?.trackerId === nodeData.tracker?.trackerId
@@ -266,10 +287,11 @@ const FlowBuilderSidePanel: any = ({ className }: any) => {
 
   useEffect(() => {
     if (nodeData.type !== NodeType.TRACKER || !selectedNode) return;
+    setNodeData({ ...nodeData, needsCheck: false });
   }, [nodeData]);
 
   useEffect(() => {
-    setSelectedNode(nodes.find((node: any) => node.selected));
+    setSelectedNode(nodes.find((node) => node.selected));
   }, [nodes]);
 
   const isOpen =
@@ -280,7 +302,7 @@ const FlowBuilderSidePanel: any = ({ className }: any) => {
 
   return (
     <>
-    {isOpen && (
+      {isOpen && (
         <div
           id="flow-builder-sidepanel-cancel"
           className="fixed top-[60px] left-0 bg-[#111827] bg-opacity-20 w-full h-screen"
@@ -378,10 +400,76 @@ const FlowBuilderSidePanel: any = ({ className }: any) => {
         <div className="h-full relative flex flex-col justify-stretch">
           <div className="p-5 border-b-[1px] flex flex-col gap-[5px]">
             <div className="font-inter flex items-center font-semibold text-[20px] leading-[28px]">
+              {selectedNode?.type
+                ? (selectedNode.data.type === NodeType.MESSAGE ||
+                    selectedNode?.data.type === NodeType.PUSH) &&
+                  (selectedNode.data as MessageNodeData).template
+                  ? (selectedNode.data as MessageNodeData).customName ||
+                    messageFixtures[
+                      (selectedNode.data as MessageNodeData).template.type
+                    ].text
+                  : nodeTypeToNameMap[selectedNode.type as NodeType]
+                : ""}
+              {(selectedNode?.data.type === NodeType.MESSAGE ||
+                selectedNode?.data.type === NodeType.PUSH) && (
+                <>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    className="ml-[10px] cursor-pointer"
+                    onClick={() => setMessageRenameModalShow(true)}
+                  >
+                    <g clipPath="url(#clip0_192_30125)">
+                      <path
+                        d="M3.45872 12.2841C3.49443 12.2841 3.53015 12.2805 3.56586 12.2752L6.56943 11.7484C6.60515 11.7412 6.63908 11.7252 6.66408 11.6984L14.2337 4.12874C14.2503 4.11222 14.2634 4.0926 14.2724 4.071C14.2813 4.0494 14.2859 4.02624 14.2859 4.00285C14.2859 3.97946 14.2813 3.95631 14.2724 3.9347C14.2634 3.9131 14.2503 3.89348 14.2337 3.87696L11.2659 0.907316C11.2319 0.873387 11.1873 0.85553 11.1391 0.85553C11.0909 0.85553 11.0462 0.873387 11.0123 0.907316L3.44265 8.47696C3.41586 8.50374 3.39979 8.53589 3.39265 8.5716L2.86586 11.5752C2.84849 11.6708 2.8547 11.7693 2.88395 11.862C2.91319 11.9547 2.9646 12.0389 3.03372 12.1073C3.15158 12.2216 3.29979 12.2841 3.45872 12.2841ZM4.66229 9.16982L11.1391 2.69482L12.448 4.00374L5.97122 10.4787L4.38372 10.7591L4.66229 9.16982ZM14.5712 13.7841H1.42836C1.11229 13.7841 0.856934 14.0395 0.856934 14.3555V14.9984C0.856934 15.077 0.921219 15.1412 0.999791 15.1412H14.9998C15.0784 15.1412 15.1426 15.077 15.1426 14.9984V14.3555C15.1426 14.0395 14.8873 13.7841 14.5712 13.7841Z"
+                        fill="#111827"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_192_30125">
+                        <rect width="16" height="16" fill="white" />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  <FlowBuilderMessageRenameModal
+                    isOpen={messageRenameModalShow}
+                    onClose={() => setMessageRenameModalShow(false)}
+                    selectedNode={selectedNode}
+                  />
+                </>
+              )}
               <br />
             </div>
             <div className="font-inter font-normal text-[12px] leading-5 text-[#4B5563]">
-          
+              {(() => {
+                switch (selectedNode?.data.type) {
+                  case NodeType.MESSAGE:
+                    return "Users in this step will receive a message with the following template";
+                  case NodeType.PUSH:
+                    return "Users in this step will receive a message with the following template";
+                  case NodeType.START:
+                    return "";
+                  case NodeType.JUMP_TO:
+                    return "Users who enter this step will jump to another step in this journey";
+                  case NodeType.EXIT:
+                    return "Users who enter this step will exit the journey";
+                  case NodeType.WAIT_UNTIL:
+                    return "Users on this trigger will move to another step only after an event, or some amount of time";
+                  case NodeType.TIME_DELAY:
+                    return "Users on this trigger will move to another step only after a time delay";
+                  case NodeType.TIME_WINDOW:
+                    return "Users on this trigger will move to another step only during a time window";
+                  case NodeType.USER_ATTRIBUTE:
+                    return "Users on this trigger will move to another step only if they satisfy some user attribute condition";
+                  case NodeType.MULTISPLIT:
+                    return "Description Description Description";
+                  default:
+                    return "";
+                }
+              })()}
             </div>
           </div>
           <div className="h-full max-h-full mb-[60px] overflow-y-hidden">
